ea12c01f21875a69160f3c83c14582e50c41d852
Kirill Klemazov <klemazov_kirill@mail.ru>
2025-08-29 13:19:19 +0300

    some changes and experiments

--- diff_kinetic.jl
+++ diff_kinetic.jl
@@ -1,16 +1,24 @@
 using DifferentialEquations
 using .kinetic_base
-
+using Plots
 
 #example solving diff eqa
 
 function F1d(u, p, t)
     Eₐ, A, β, = p;
-    K(A, Eₐ,β*t)*F1(u)     
+    K(A, Eₐ,β*t)*F2(u)     
 end
 
-tspan = (0,1);
-u0 = 1e-2;
-p = [1.0, 100.0, 20.];
-prob = ODEProblem(F1d, u0,tspan, p);
+function F1B1(du, u, p, t)
+    Eₐ, A, β, = p;
+    du[1] = K(A, Eₐ, β*t)*F1(u[1]); 
+    du[2] = K(A, Eₐ, β*t)*B1(du[1]);    
+end
+
+tspan = (0,10);
+u0 = [1e-2, 1e-2];
+p = [1.0, 1, 1];
+prob = ODEProblem(F1B1, u0, tspan, p);
 sol = solve(prob);
+sol
+plot(sol)
--- kinetic_base.jl
+++ kinetic_base.jl
@@ -1,26 +1,25 @@
 module kinetic_base
-export R, K, F1, Fn, B1, Bna,C1, Cn, Cnm
+export R, K, F1, F2, Fn, B1, Bna,C1, Cn, Cnm
 
 const R = 8.314;
 
 # Constant of reaction
-K(A::Number,Eₐ::Number,T::Number) = A*exp(-Eₐ/(R*T));
+K(A,Eₐ,T) = A*exp(-Eₐ/(R*T));
 
 
 
 # for solving via diff equations
-F1(α) = @. 1- α; #first order
+F1(α) = @. (1.0 .- α); #first order
+F2(α) = @. (1.0 - α)^2; #second order
 
-F2(α) = (1- α)^2; #second order
-
-Fn(α,n) = (1-α)^n; #n-th order
+Fn(α,n) = (1.0-α)^n; #n-th order
 
 
-B1(α) = (1-α)*α; # Prout-Tompkins equation
+B1(α) = (1 .- α)*α; # Prout-Tompkins equation
 
-Bna(α,n,m) = (1-α)^n*α^m; # the extended Prout-Tompkins equation
+Bna(α,n,m) = @. (1. -α)^n*α^m; # the extended Prout-Tompkins equation
 
-C1(α, Kcat) = F1(α)*(1+Kcat*α) #Kama-Sourour auto-catalysis reaction n = 1, m = 1
+C1(α, Kcat) = @. F1(α)*(1.0 +Kcat*α) #Kama-Sourour auto-catalysis reaction n = 1, m = 1
 
 Cn(α,n, Kcat) = Fn(α,n)*(1+Kcat*α) #Kama-Sourour auto-catalysis reaction m = 1
 
